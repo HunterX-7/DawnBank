b5d7e5e84c5b84a7da1c5dcaaaea9f58a2019891
HunterX-7 <fraquin7@gmail.com>
2023-07-21 18:16:10 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#00040f",
-        secondary: "#00f6ff",
+        secondary: "#1F62FF",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
@@ -16,7 +16,7 @@ module.exports = {
     },
     screens: {
       xs: "480px",
-      ss: "620px",
+      ss: "630px",
       sm: "768px",
       md: "1060px",
       lg: "1200px",
