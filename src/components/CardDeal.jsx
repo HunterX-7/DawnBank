8e5fb0e18fafa3900ef22d0ccb009718f7646f38
HunterX-7 <fraquin7@gmail.com>
2023-07-23 00:15:22 -0500

    Add and style the creditcard section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          Discover the best card offers <br className='sm:block hidden' /> effortlessly with a simple process.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px]`}>
+          Uncover the best card deals through our user-friendly steps, ensuring you find the ideal match for your financial needs.
+        </p>
+        <Button styles='mt-10' />
+      </div>
+      <div className={`${layout.sectionImg}`}>
+        <img src={card} alt="card-deal" className='w-[100%] h-[100%]' />
+      </div>
+    </section>
   )
 }
 
