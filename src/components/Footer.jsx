f5b263807f6a45c1e5823eed65e7dd8e73f3ce91
HunterX-7 <fraquin7@gmail.com>
2023-07-24 23:52:07 -0500

    Add and style the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React from 'react';
+import styles from '../style';
+import { logo } from '../assets';
+import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <section className={`${styles.flexCenter} sm:py-10 py-6 flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className='flex-1 flex flex-col justify-start mr-10'>
+          <img src={logo} alt="dawnbank" className='w-[266px] h-[72px] object-contain' />
+        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+          Seamless Payments Redefined: Simplifying Transactions, Accelerating Progress, Future-Ready Solution.
+        </p>
+        </div>
+        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+          {footerLinks.map((link) => (
+            <div key={link.title} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
+              <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
+                {link.title}
+              </h4>
+              <ul className='list-none mt-4'>
+                {link.links.map((footerLink, index) => (
+                  <li 
+                    key={footerLink.name} 
+                    className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer 
+                      ${index !== link.links.length-1 ? 'mb-4' : 'mb-0'}
+                    `}
+                  >
+                    {footerLink.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[grey]'>
+        <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+          Copyright &copy; {currentYear} DawnBank. All Rights Reserved.
+        </p>
+        <div className='flex flex-row md:mt-0 mt-6'>
+          {socialMedia.map((social, index) => (
+            <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer 
+              ${index !== socialMedia.length ? 'mr-6' : 'mr-0'}
+            `} />
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
